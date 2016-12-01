9ae7c0c7488bbdab99c36b6c8158a63c32e0fa97
peio <pziarsolo@upv.es>
2016-12-01 16:50:43 +0100

    remove signal connections at startup, cache sistem already takes into account the changes

--- vavilov/apps.py
+++ vavilov/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 from django.conf import settings
-from django.db.models import signals
 
 
 class VavilovAppConfig(AppConfig):
@@ -9,12 +8,5 @@ class VavilovAppConfig(AppConfig):
     def ready(self):
         if not settings.DEBUG:
             from vavilov.caches import get_passport_data_choices, get_taxons
-            from vavilov.models import AccessionTaxa, Passport
-
             get_taxons()
             get_passport_data_choices()
-
-            signals.post_save.connect(receiver=get_taxons,
-                                      sender=AccessionTaxa)
-            signals.post_save.connect(receiver=get_passport_data_choices,
-                                      sender=Passport)
