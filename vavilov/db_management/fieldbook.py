604d3228b638be45380c3f4aafa81e63ff25584a
peio <pziarsolo@upv.es>
2017-01-11 15:02:37 +0000

    added new line at end of file

--- vavilov/db_management/fieldbook.py
+++ vavilov/db_management/fieldbook.py
@@ -228,3 +228,4 @@ def insert_newest_observations(fhand, plants, excluded_traits=None,
                      local_creation_time, observation.observer, '', '1', '', ''))
     cur.close()
     con.commit()
+
