d5cd0aae8262ec9c7a5b633962e54c0abea94d04
peio <pziarsolo@upv.es>
2017-05-22 11:17:49 +0200

    add view all observation permissions for users

--- vavilov/permissions.py
+++ vavilov/permissions.py
@@ -1,13 +1,11 @@
-from django.dispatch.dispatcher import receiver
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User, Permission
-from django.core.exceptions import AppRegistryNotReady
+from django.contrib.auth.models import Permission
 
 
 VIEW_PERMISIONS = ('View Accession', 'View Passport', 'View Location',
                    'View Accession Relationship', 'View Accession Taxa',
                    'View Assay', 'View Plant', 'View AssayProp',
-                   'View Observation')
+                   'View Observation', 'View observation images',
+                   'View Observation Relationship', 'View Observation entity')
 
 
 def add_view_permissions(user, filter_perms=None):
@@ -16,17 +14,3 @@ def add_view_permissions(user, filter_perms=None):
             continue
         permission = Permission.objects.get(name=perm_name)
         user.user_permissions.add(permission)
-
-
-# All users created have view permission for models.
-# Per row permission must be granted
-# When we create new user we recevive the signal and give to the user view
-# permissions
-@receiver(post_save, sender=User)
-def add_def_view_perms(sender, instance, **kwargs):
-    try:
-        add_view_permissions(instance)
-    except Permission.DoesNotExist as error:
-        if str(error) == 'Permission matching query does not exist.':
-            raise AppRegistryNotReady('guardian loaded before vavilov loaded')
-        raise
--- vavilov/signals.py
+++ vavilov/signals.py
@@ -2,9 +2,27 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from vavilov.permissions import add_view_permissions
+from django.contrib.auth.models import Permission, User
+from django.core.exceptions import AppRegistryNotReady
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+# All users created have view permission for models.
+# Per row permission must be granted
+# When we create new user we recevive the signal and give to the user view
+# permissions
+
+
+@receiver(post_save, sender=User)
+def add_def_view_perms(sender, instance, **kwargs):
+    try:
+        add_view_permissions(instance)
+    except Permission.DoesNotExist as error:
+        if str(error) == 'Permission matching query does not exist.':
+            raise AppRegistryNotReady('guardian loaded before vavilov loaded')
+        raise
