cc6f9514953bf62fa22503f7ec1c451bfc5fb052
peio <pziarsolo@upv.es>
2018-09-12 10:57:18 +0200

    update pedigree upload code

--- vavilov_pedigree/management/commands/pedigree_load_data.py
+++ vavilov_pedigree/management/commands/pedigree_load_data.py
@@ -1,13 +1,13 @@
+from os.path import join
 from time import time
 from django.core.management.base import BaseCommand
 
 from vavilov_pedigree.data_upload import (add_or_load_accessions,
                                           add_or_load_seedlot,
-                                          add_or_load_plant_relationship,
-                                          add_or_load_cross_experiments,
-                                          add_or_load_plants,
                                           add_or_load_crosses_data)
-from os.path import join
+#                                           add_or_load_plant_relationship,
+#                                           add_or_load_cross_experiments,
+#                                           add_or_load_plants,
 
 
 def print_duration(msg, prev_time):
@@ -34,46 +34,49 @@ class Command(BaseCommand):
         add_or_load_seedlot(join(dirpath, 'SeedLots_originals.xlsx'))
         prev_time = print_duration('SeedLots_originals.xlsx', prev_time)
 
-        add_or_load_crosses_data(join(dirpath, 'varitome_crosses.xlsx'),
+        add_or_load_seedlot(join(dirpath, 'SeedLots_magic.xlsx'))
+        prev_time = print_duration('SeedLots_magic.xlsx', prev_time)
+
+        add_or_load_crosses_data(join(dirpath, 'varitome_crosses_all.xlsx'),
                                  only_check=only_check)
-        add_or_load_crosses_data(join(dirpath, 'varitome_crosses_spring2018.xlsx'),
-                                 only_check=only_check)
+#         add_or_load_crosses_data(join(dirpath, 'varitome_crosses_spring2018.xlsx'),
+#                                  only_check=only_check)
 
-        return
-        add_or_load_plants(join(dirpath, 'NSF2_PA01_Sep_2016_plants.xlsx'))
-        prev_time = print_duration('NSF2_PA01_Sep_2016_plants.xlsx', prev_time)
-
-        add_or_load_plants(join(dirpath, 'NSF2_CN0X_Feb_2016_plants.xlsx'))
-        prev_time = print_duration('NSF2_CN0X_Feb_2016_plants.xlsx', prev_time)
-
-        add_or_load_plants(join(dirpath, 'NSF1_CN0X_Feb_2016_plants.xlsx'))
-        prev_time = print_duration('NSF1_CN0X_Feb_2016_plants.xlsx', prev_time)
-
-        add_or_load_plant_relationship(join(dirpath, 'NSF2_PA01_Sep_2016_plant_clones.xlsx'))
-        prev_time = print_duration('NSF2_PA01_Sep_2016_plant_clones.xlsx', prev_time)
-
-        add_or_load_plant_relationship(join(dirpath, 'NSF2_CN0X_Feb_2016_plant_clones.xlsx'))
-        prev_time = print_duration('NSF2_CN0X_Feb_2016_plant_clones.xlsx', prev_time)
-
-        add_or_load_cross_experiments(join(dirpath, 'Crosses_F16NSF2.xlsx'))
-        prev_time = print_duration('Crosses_F16NSF2.xlsx', prev_time)
-
-        add_or_load_cross_experiments(join(dirpath, 'Crosses_F16NSF3_upv.xlsx'))
-        prev_time = print_duration('Crosses_F16NSF3_upv.xlsx', prev_time)
-
-        # 2017 March
-        dirpath_2017march = join(dirpath, 'SEP_2017')
-        add_or_load_plants(join(dirpath_2017march, 'NSF2_PA01_March_2017_plants.xlsx'))
-        prev_time = print_duration('NSF2_PA01_March_2017_plants.xlsx', prev_time)
-
-        add_or_load_plants(join(dirpath_2017march, 'NSF3_AQ01_March_2017_plants.xlsx'))
-        prev_time = print_duration('NSF3_AQ01_March_2017_plants.xlsx', prev_time)
-
-        add_or_load_cross_experiments(join(dirpath_2017march, 'NSF2_PA01_March_2017_crosses.xlsx'))
-        prev_time = print_duration('NSF2_PA01_March_2017_crosses.xlsx', prev_time)
-
-        add_or_load_cross_experiments(join(dirpath_2017march, 'NSF3_AQ01_March_2017_crosses.xlsx'))
-        prev_time = print_duration('NSF3_AQ01_March_2017_crosses.xlsx', prev_time)
-
-        add_or_load_cross_experiments(join(dirpath_2017march, 'CRUCES_ESPECIFICOS-sep2017.xlsx'))
-        prev_time = print_duration('CRUCES_ESPECIFICOS-sep2017.xlsx', prev_time)
+#         return
+#         add_or_load_plants(join(dirpath, 'NSF2_PA01_Sep_2016_plants.xlsx'))
+#         prev_time = print_duration('NSF2_PA01_Sep_2016_plants.xlsx', prev_time)
+#
+#         add_or_load_plants(join(dirpath, 'NSF2_CN0X_Feb_2016_plants.xlsx'))
+#         prev_time = print_duration('NSF2_CN0X_Feb_2016_plants.xlsx', prev_time)
+#
+#         add_or_load_plants(join(dirpath, 'NSF1_CN0X_Feb_2016_plants.xlsx'))
+#         prev_time = print_duration('NSF1_CN0X_Feb_2016_plants.xlsx', prev_time)
+#
+#         add_or_load_plant_relationship(join(dirpath, 'NSF2_PA01_Sep_2016_plant_clones.xlsx'))
+#         prev_time = print_duration('NSF2_PA01_Sep_2016_plant_clones.xlsx', prev_time)
+#
+#         add_or_load_plant_relationship(join(dirpath, 'NSF2_CN0X_Feb_2016_plant_clones.xlsx'))
+#         prev_time = print_duration('NSF2_CN0X_Feb_2016_plant_clones.xlsx', prev_time)
+#
+#         add_or_load_cross_experiments(join(dirpath, 'Crosses_F16NSF2.xlsx'))
+#         prev_time = print_duration('Crosses_F16NSF2.xlsx', prev_time)
+#
+#         add_or_load_cross_experiments(join(dirpath, 'Crosses_F16NSF3_upv.xlsx'))
+#         prev_time = print_duration('Crosses_F16NSF3_upv.xlsx', prev_time)
+#
+#         # 2017 March
+#         dirpath_2017march = join(dirpath, 'SEP_2017')
+#         add_or_load_plants(join(dirpath_2017march, 'NSF2_PA01_March_2017_plants.xlsx'))
+#         prev_time = print_duration('NSF2_PA01_March_2017_plants.xlsx', prev_time)
+#
+#         add_or_load_plants(join(dirpath_2017march, 'NSF3_AQ01_March_2017_plants.xlsx'))
+#         prev_time = print_duration('NSF3_AQ01_March_2017_plants.xlsx', prev_time)
+#
+#         add_or_load_cross_experiments(join(dirpath_2017march, 'NSF2_PA01_March_2017_crosses.xlsx'))
+#         prev_time = print_duration('NSF2_PA01_March_2017_crosses.xlsx', prev_time)
+#
+#         add_or_load_cross_experiments(join(dirpath_2017march, 'NSF3_AQ01_March_2017_crosses.xlsx'))
+#         prev_time = print_duration('NSF3_AQ01_March_2017_crosses.xlsx', prev_time)
+#
+#         add_or_load_cross_experiments(join(dirpath_2017march, 'CRUCES_ESPECIFICOS-sep2017.xlsx'))
+#         prev_time = print_duration('CRUCES_ESPECIFICOS-sep2017.xlsx', prev_time)
